fb97ed23a67c4abd3171dec396101f2f082f6990
Brian Reece <bdreece@mtu.edu>
2022-04-15 12:49:37 -0400

    Implemented message context and factory

---
+++ pkg/context/message/factory.go
@@ -0,0 +1,11 @@
+package message
+
+import _ "github.com/bdreece/tattle/pkg/format"
+
+type Factory[F Format] struct {
+	builder Builder[Context, F]
+}
+
+func (f Factory) Create(ctx Context) string {
+	return f.builder.Build(ctx).Format()
+} 
---
+++ pkg/context/message/go.mod
@@ -0,0 +1,3 @@
+module github.com/bdreece/tattle/pkg/context/message
+
+go 1.18
